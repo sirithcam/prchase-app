dc20f14ccf26bf49ed0a459e906ac4458524df0a
Mateusz Czuchryta <mateusz.czuchryta@gmail.com>
2023-08-16 16:31:14 +0200

    finish purchases

--- app/controllers/api/purchase_intents_controller.rb
+++ app/controllers/api/purchase_intents_controller.rb
@@ -20,10 +20,31 @@ class Api::PurchaseIntentsController < ApplicationController
   end
 
   def destroy
+    @purchase_intent = PurchaseIntent.find(params[:id])
+
     if @purchase_intent.destroy
       head :no_content
     else
       render json: { errors: @purchase_intent.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def process_purchase
+    puts "TOKEN:"
+    puts params[:token]
+    @purchase_intent = PurchaseIntent.find_by(token: params[:token])
+    puts "INTENT"
+    puts @purchase_intent.attributes
+
+    if @purchase_intent.nil?
+      render json: { error: 'Purchase intent not found' }, status: :not_found
+    elsif @purchase_intent.status != 'pending'
+      render json: { error: 'Purchase intent has already been processed' }, status: :unprocessable_entity
+    elsif purchase_intent_attributes_match?
+      @purchase_intent.update(status: 'done')
+      render json: { message: 'Book has been purchased' }, status: :ok
+    else
+      render json: { error: 'Purchase intent attributes do not match' }, status: :unprocessable_entity
     end
   end
 
@@ -43,5 +64,23 @@ class Api::PurchaseIntentsController < ApplicationController
     unless valid_currencies.include?(currency) && valid_payment_methods.include?(payment_method)
       render json: { error: 'Invalid currency or payment method' }, status: :unprocessable_entity
     end
+  end
+
+  def purchase_intent_attributes_match?
+    payload_attributes = {
+      "user_id" => params[:user_id].to_i,
+      "book_id" => params[:book_id].to_i,
+      "price" => params[:price].to_f,
+      "currency" => params[:currency],
+      "payment_method" => params[:payment_method]
+    }
+
+    puts "ATTRIBUTES"
+    puts payload_attributes
+
+    puts "INTENT"
+    puts @purchase_intent.attributes.slice(*payload_attributes.keys.map(&:to_s))
+
+    @purchase_intent.attributes.slice(*payload_attributes.keys.map(&:to_s)) == payload_attributes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :purchase_intents, only: %i[create destroy show index]
+
+    resources :purchases, only: [:index, :create]
+
+    post '/purchase_intents/process_purchase', to: 'purchase_intents#process_purchase'
   end
 end
